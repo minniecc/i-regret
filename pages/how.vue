1cb100d88d0cf66464554d468b57a8ea887f7283
Chantchanok Chantpakpimon <chantchanokchantpakpimon@Chantchanoks-MacBook-Pro.local>
2018-11-30 17:05:55 +0700

    Fresh start bb

---
+++ pages/how.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="container _col-12 _tal-ct">
+    <div class="how col-12">
+      <h1>Death-related Projects</h1>
+      <p>about my topic breifly how did i come up with the topic etc Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat inventore, enim illo earum ipsum suscipit perspiciatis voluptate eligendi accusantium assumenda rerum autem, voluptas aut officia maxime quas, animi fugiat qui!</p>
+    </div>
+    <!-- 1 -->
+    <div class="row box col-lg-8 col-md-8 col-10 _dp-b">
+      <h2>Death's story</h2>
+      <div class="col-12 pic pic1"></div>
+      <div class="abouttext col-12 _tal-l">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad enim blanditiis facilis laboriosam sit sequi laudantium facere, ut provident impedit, deserunt, cum non accusantium quis odit omnis veniam voluptatem minus?</p>
+        <br>
+        <p>
+          Download the form
+          <a
+            style="color: white; text-decoration: underline;"
+            href="/pdf/death-story.pdf"
+            download
+          >here</a>.
+        </p>
+      </div>
+    </div>
+    <!-- 2 -->
+    <div class="row box col-lg-8 col-md-8 col-10 _dp-b">
+      <h2>Design your funeral</h2>
+      <div class="col-12 pic pic2"></div>
+      <div class="abouttext col-12 _tal-l">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere vero eaque blanditiis animi quo numquam alias minus cupiditate! Totam fugit odit veritatis accusantium consequuntur officia itaque. Minima ipsum iusto facere!</p>
+        <br>
+        <p>
+          Try it
+          <a
+            style="color: white; text-decoration: underline;"
+            href="https://myfuneralpicture.firebaseapp.com/funeralpic2/"
+            target="_blank"
+          >here</a>.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 0.2em;
+  font-size: 30px;
+  font-weight: 600;
+  padding: 3% 3%;
+  color: black;
+}
+
+h2 {
+  text-align: left;
+  text-transform: uppercase;
+  letter-spacing: 0.2em;
+  padding: 10px;
+  font-size: 20px;
+  font-weight: 600;
+  color: white;
+}
+
+.box {
+  background-color: black;
+  padding: 20px;
+  margin: 30px auto;
+  > p {
+    color: white;
+    text-align: left;
+  }
+}
+
+.abouttext {
+  margin: 10px auto;
+  > a {
+    color: white;
+    text-align: left;
+  }
+  > p {
+    color: white;
+    text-align: left;
+  }
+}
+
+.pic {
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  transition: 1s;
+}
+
+.pic1 {
+  background-image: url("~assets/images/otherpic/death-story.png");
+}
+.pic2 {
+  background-image: url("~assets/images/otherpic/death-story.png");
+}
+</style>
+
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,73 @@
 <template>
-  <div class="_w-100pct">
-    <!-- Hero -->
-    <div class="container">
-      <div class="row">
-        <div class="col-12">BYE</div>
+  <div class="_w-100pct container">
+    <!-- filters -->
+    <div class="row filter col-lg-10 col-md-10 col-sm-12 col-12 _fs-6 _jtfct-spbtw">
+      <div class="_dp-ilb filter" v-for="(item, i) in regretCategories" :key="i">
+        <i class="fas fa-circle" :style="`color: ${item.color}`"></i>
+        <span>{{ item.title }}</span>
+      </div>
+    </div>
+
+    <div class="row playarea">
+      <!-- <div class="col-lg-1 col-md-2 col-sm-2 col-2 _dp-b _mg-at toolsbar _tal-ct">
+
+        <div>
+          <a href>
+            <i class="fas fa-fw fa-lg fa-search"></i>
+          </a>
+          <div class="texttool">
+            <p>Search for Regret</p>
+          </div>
+        </div>
+
+        <div @click="mousedownCanvas()">
+          <a href>
+            <i class="fas fa-fw fa-lg fa-plus-circle"></i>
+          </a>
+          <div class="texttool">
+            <p>Confess your Regret</p>
+          </div>
+        </div>
+      </div>-->
+      <div class="col col-lg-2 col-md-2 col-sm-2 col-2 _dp-b _mg-at _tal-ct popup">
+        <form method="get">
+          <!-- NAME INPUT -->
+          <div class="form">
+            <label for="name">Name</label>
+            <input type="name" id="name" placeholder="annonymous">
+          </div>
+
+          <!-- REGRET TEXT INPUT -->
+          <div class="form">
+            <label for="textregret">I regret ...</label>
+            <input type="text" class="textregret" name="textregret">
+          </div>
+
+          <!-- CATEGORY -->
+          <div class="form">
+            <label for="regretcategory">Category</label>
+            <div class="bio-field -info">
+              <div class="bio-select">
+                <select>
+                  <option disabled>Select dropdown</option>
+                  <option v-for="(item, i) in regretCategories" :key="i">{{ item.title }}</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <!-- BUTTON -->
+          <button onclick="mousedownAdd()" type="submit" name="Add">Add</button>
+          <button type="reset" value="Reset">Reset</button>
+          <!-- <button><nuxt-link to="/">Cancel</nuxt-link></button> -->
+        </form>
+      </div>
+
+      <!-- d3 -->
+      <div class="col-10 _dp-b _mg-at regret">
+        <div id="d3">
+          <div id="tooltip"></div>
+        </div>
       </div>
     </div>
   </div>
@@ -11,11 +75,318 @@
 
 <script>
 export default {
-  async created() {
-    // fetch data via api
+  data: () => ({
+    regretCategories: [
+      {
+        title: "Self-worth",
+        color: "#3478AF"
+      },
+      {
+        title: "Family",
+        color: "#F08637"
+      },
+      {
+        title: "Friends",
+        color: "#4F9D3C"
+      },
+      {
+        title: "Love",
+        color: "#C63931"
+      },
+      {
+        title: "Education",
+        color: "#8B6AAC"
+      },
+      {
+        title: "Career",
+        color: "#85584F"
+      },
+      {
+        title: "Bucket List",
+        color: "#CE7EB4"
+      },
+      {
+        title: "Other",
+        color: "#7F7F7F"
+      }
+    ]
+  }),
+
+  methods: {
+    mousedownCanvas() {
+      var inputText = window.prompt("Enter your regret");
+      // var point = d3.mouse(this),
+      //   node = { x: point[0], y: point[1], text: inputText },
+      //   n = nodes.push(node);
+
+      // restart();
+    },
+    mousedownAdd() {
+      // var regretText = window.prompt("Enter your regret");
+      // var name = ...
+      print(hey);
+    }
+  },
+  mounted() {
+    if (process.browser) {
+      /* SVG frame creation */
+      var width = 960,
+        height = 500,
+        // color
+        fill = d3.scale.category10();
+      // fill = regretCategories.item.color();
+
+      this.fill = d3.scale.category10();
+      // this.fill = regretCategories.item.color();
+
+      var tooltip = d3.select("#tooltip");
+
+      var _nodes = d3.range(0).map(Object);
+
+      var force = d3.layout
+        .force()
+        .size([width, height])
+        .nodes(_nodes) // initialize with a single node
+        .linkDistance(30)
+        .charge(-60)
+        .on("tick", tick);
+
+      var svg = d3
+        .select("#d3")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        // .on("mousemove", mousemove)
+        .on("mousedown", mousedownCanvas);
+
+      var nodes = force.nodes(),
+        links = force.links(),
+        node = svg.selectAll(".node"),
+        link = svg.selectAll(".link");
+
+      restart();
+
+      //hover to show tooltip
+      function mouseover(e) {
+        console.log(e);
+        var text = e.text;
+        tooltip
+          .style("top", event.clientY - 250 + "px")
+          .style("left", event.clientX - 350 + "px")
+          .style("opacity", 1)
+          .html(text);
+      }
+
+      function mouseout() {
+        tooltip
+          .style("top", event.clientY + "px")
+          .style("left", event.clientX + "px")
+          .style("opacity", 0);
+      }
+
+      function mousedownCanvas() {
+        var inputText = window.prompt("I regret");
+        // var inputText = textregret;
+        var point = d3.mouse(this),
+          node = {
+            x: point[0],
+            y: point[1],
+            text: "I regret " + inputText + " - Name"
+          },
+          n = nodes.push(node);
+
+        restart();
+      }
+
+      function tick() {
+        link
+          .attr("x1", function(d) {
+            return d.source.x;
+          })
+          .attr("y1", function(d) {
+            return d.source.y;
+          })
+          .attr("x2", function(d) {
+            return d.target.x;
+          })
+          .attr("y2", function(d) {
+            return d.target.y;
+          });
+
+        node
+          .attr("cx", function(d) {
+            return d.x;
+          })
+          .attr("cy", function(d) {
+            return d.y;
+          });
+      }
+
+      function restart() {
+        node = node.data(nodes);
+
+        //dots style
+        node
+          .enter()
+          .insert("circle", ".cursor")
+          .attr("class", "node")
+          .attr("regret", "abc")
+          .attr("r", 5)
+          // color
+          .style("fill", function(d, i) {
+            return fill(i & 7);
+          })
+          .style("stroke", function(d, i) {
+            return d3.rgb(fill(i & 7)).brighter(1);
+          })
+          .style("stroke-width", 1.5)
+          .on("mouseover", mouseover)
+          .on("mouseout", mouseout);
+        // .on("mousedown", mousedownNode);
+
+        node.exit().remove();
+
+        force.start();
+      }
+    }
   }
 };
 </script>
 
+
+
+
+
 <style lang="scss" scoped>
+.filter {
+  margin: auto 0 auto auto;
+  padding: 10px;
+  font-family: "Work Sans", sans-serif;
+}
+
+.playarea {
+  margin-bottom: 50px;
+  font-family: "Work Sans", sans-serif;
+  background-color: #3c3c3b;
+}
+
+.regret {
+  background: black;
+}
+
+.toolsbar {
+  background-color: black;
+  position: relative;
+  padding: 10px;
+  z-index: 100;
+  & a {
+    color: white;
+    &:hover {
+      color: #98c1d9;
+    }
+  }
+  & i {
+    margin: 10px 0px;
+  }
+  & p {
+    font-size: 16px;
+    color: #98c1d9;
+  }
+}
+
+.texttool {
+  height: 100%;
+  width: 100%;
+  color: white;
+  display: block;
+  opacity: 0;
+  transition: opacity 0.2s, visibility 0.2s;
+}
+
+.toolsbar:hover .texttool {
+  opacity: 1;
+}
+
+//d3
+#tooltip {
+  position: absolute;
+  background: #98c1d9;
+  top: 250px;
+  left: 500px;
+  // transition: 0.1s;
+  opacity: 0;
+  padding: 24px;
+}
+
+rect {
+  fill: none;
+  pointer-events: all;
+}
+
+.node {
+  fill: #000;
+}
+
+// popup
+.popup {
+  background: #3c3c3b;
+  color: white;
+  text-transform: uppercase;
+  font-size: 15px;
+  padding: 10px;
+  height: 100%;
+  width: 100%;
+  left: 0;
+  top: 0;
+
+  > div {
+    position: fixed;
+    background: white;
+    box-shadow: 0px 0px 15px 0px rgba(#000000, 0.3);
+    padding: 30px 15px;
+    /* Width of popup can be changed */
+    width: 70%;
+    max-width: 600px;
+    z-index: 5001;
+    left: 50%;
+    top: 50%;
+    text-align: center;
+    border: 5px solid #98c1d9;
+  }
+}
+
+.form {
+  margin-bottom: 10px;
+}
+
+input {
+  border-radius: 5px;
+  border: none;
+  height: 30px;
+}
+
+select {
+  border: none;
+}
+
+//button
+button {
+  background: #98c1d9;
+  border: 0;
+  border-radius: 4px;
+  margin: 10px 5px;
+  padding: 7px 15px;
+  font-size: 16px;
+  color: #ffffff;
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    background: lighten(#98c1d9, 10%);
+  }
+}
 </style>
+
