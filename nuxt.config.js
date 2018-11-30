d2ad83e125eb4a78949dc479b92995bdee60219d
Chantchanok Chantpakpimon <chantchanokchantpakpimon@Chantchanoks-MacBook-Pro.local>
2018-11-30 16:53:38 +0700

    - first commit

---
+++ nuxt.config.js
@@ -0,0 +1,179 @@
+const pkg = require('./package')
+const messages = require('./services/lang')
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
+
+module.exports = {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: `${pkg.title} – ${pkg.description}`,
+    meta: [{
+      charset: 'utf-8'
+    }, {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no'
+    }, {
+      hid: 'og:type',
+      property: 'og:type',
+      content: 'website'
+    }, {
+      hid: 'title',
+      name: 'title',
+      content: `${pkg.title}`
+    }, {
+      hid: 'description',
+      name: 'description',
+      content: pkg.description
+    }, {
+      hid: 'og:title',
+      name: 'og:title',
+      content: `${pkg.title}`
+    }, {
+      hid: 'og:description',
+      name: 'og:description',
+      content: pkg.description
+    }, {
+      hid: 'og:image',
+      property: 'og:image',
+      content: ``
+    }],
+    script: [{
+      src: '/js/offline.min.js',
+      body: true
+    }],
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: {
+    color: '#3B8070'
+  },
+
+  /*
+   ** Global CSS
+   */
+  css: [{
+    src: 'assets/styles/main.scss',
+    lang: 'scss'
+  }],
+  router: {
+    middleware: ['router']
+  },
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [
+    '~/plugins/ssr.js',
+    '~/plugins/axios.js',
+    {
+      src: '~/plugins/no-ssr.js',
+      ssr: false
+    }
+  ],
+  render: {
+    resourceHints: false
+  },
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    [
+      'nuxt-i18n',
+      {
+        seo: false,
+        strategy: 'prefix_except_default',
+        locales: [{
+            code: 'th',
+            iso: 'th-TH',
+            name: 'TH'
+          },
+          {
+            code: 'en',
+            iso: 'en-US',
+            name: 'EN'
+          }
+        ],
+        detectBrowserLanguage: false,
+        defaultLocale: 'th',
+        vueI18n: {
+          fallbackLocale: 'th',
+          messages
+        }
+      }
+    ],
+    [
+      'nuxt-rfg-icon', {
+        masterPicture: 'http://d1ujqdpfgkvqfi.cloudfront.net/favicon-generator/htdocs/favicons/2014-10-19/3d686326fc83540b583856510c4c9e9c.ico'
+      }
+    ],
+    'nuxt-babel',
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios',
+    // ['@nuxtjs/google-analytics', {
+    //   id: '',
+    //   autoTracking: {
+    //     pageviewOnLoad: false,
+    //     exception: true
+    //   }
+    // }],
+  ],
+  /*
+   ** Axios module configuration
+   */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+    credentials: true,
+    debug: false,
+    https: true,
+    port: 443,
+    host: pkg.apiUrl
+    // prefix: '/wp-json/api/v1', moved to apiUrl.js
+  },
+  /*
+   ** Build configuration
+   */
+  buildDir: 'nuxt',
+  build: {
+    publicPath: '/',
+    parallel: true,
+    plugins: [
+      new MomentLocalesPlugin({
+        localesToKeep: ['en', 'th']
+      })
+    ],
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.client) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+      if (!ctx.isDev) {
+        // Remove unused CSS using purgecss. See https://github.com/FullHuman/purgecss
+        // for more information about purgecss.
+        config.plugins.push(
+          new PurgecssPlugin({
+            paths: glob.sync([
+              path.join(__dirname, './pages/**/*.vue'),
+              path.join(__dirname, './layouts/**/*.vue'),
+              path.join(__dirname, './components/**/*.vue')
+            ]),
+            whitelist: ['html', 'body']
+          })
+        )
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="_w-100pct">
+    <!-- Hero -->
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ullam officiis ab fugit perspiciatis aliquid doloribus dicta rem deleniti magnam iure corporis tempore reiciendis perferendis fuga odit praesentium, eaque ipsa?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, omnis ipsum eius quis explicabo inventore possimus similique perferendis harum id numquam esse sequi! Commodi harum neque eveniet quam laborum qui.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias at ratione ad non odio, reiciendis veniam doloremque vero consequatur optio pariatur eligendi? Laudantium qui numquam tempora modi, necessitatibus consequuntur quasi?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ullam officiis ab fugit perspiciatis aliquid doloribus dicta rem deleniti magnam iure corporis tempore reiciendis perferendis fuga odit praesentium, eaque ipsa?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, omnis ipsum eius quis explicabo inventore possimus similique perferendis harum id numquam esse sequi! Commodi harum neque eveniet quam laborum qui.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias at ratione ad non odio, reiciendis veniam doloremque vero consequatur optio pariatur eligendi? Laudantium qui numquam tempora modi, necessitatibus consequuntur quasi?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ullam officiis ab fugit perspiciatis aliquid doloribus dicta rem deleniti magnam iure corporis tempore reiciendis perferendis fuga odit praesentium, eaque ipsa?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, omnis ipsum eius quis explicabo inventore possimus similique perferendis harum id numquam esse sequi! Commodi harum neque eveniet quam laborum qui.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias at ratione ad non odio, reiciendis veniam doloremque vero consequatur optio pariatur eligendi? Laudantium qui numquam tempora modi, necessitatibus consequuntur quasi?
+          x
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async created () {
+    // fetch data via api
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
