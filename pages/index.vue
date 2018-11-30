d676ce36af06eb6c43a61ac7cba0106482fbed5d
Chantchanok Chantpakpimon <chantchanokchantpakpimon@Chantchanoks-MacBook-Pro.local>
2018-11-30 16:57:14 +0700

    yay

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,7 @@
     <!-- Hero -->
     <div class="container">
       <div class="row">
-        <div class="col-12">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ullam officiis ab fugit perspiciatis aliquid doloribus dicta rem deleniti magnam iure corporis tempore reiciendis perferendis fuga odit praesentium, eaque ipsa?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, omnis ipsum eius quis explicabo inventore possimus similique perferendis harum id numquam esse sequi! Commodi harum neque eveniet quam laborum qui.
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias at ratione ad non odio, reiciendis veniam doloremque vero consequatur optio pariatur eligendi? Laudantium qui numquam tempora modi, necessitatibus consequuntur quasi?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ullam officiis ab fugit perspiciatis aliquid doloribus dicta rem deleniti magnam iure corporis tempore reiciendis perferendis fuga odit praesentium, eaque ipsa?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, omnis ipsum eius quis explicabo inventore possimus similique perferendis harum id numquam esse sequi! Commodi harum neque eveniet quam laborum qui.
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias at ratione ad non odio, reiciendis veniam doloremque vero consequatur optio pariatur eligendi? Laudantium qui numquam tempora modi, necessitatibus consequuntur quasi?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ullam officiis ab fugit perspiciatis aliquid doloribus dicta rem deleniti magnam iure corporis tempore reiciendis perferendis fuga odit praesentium, eaque ipsa?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, omnis ipsum eius quis explicabo inventore possimus similique perferendis harum id numquam esse sequi! Commodi harum neque eveniet quam laborum qui.
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias at ratione ad non odio, reiciendis veniam doloremque vero consequatur optio pariatur eligendi? Laudantium qui numquam tempora modi, necessitatibus consequuntur quasi?
-          x
-        </div>
+        <div class="col-12">BYE</div>
       </div>
     </div>
   </div>
@@ -22,10 +11,10 @@
 
 <script>
 export default {
-  async created () {
+  async created() {
     // fetch data via api
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
